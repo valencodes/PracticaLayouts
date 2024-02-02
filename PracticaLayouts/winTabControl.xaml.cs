7fccfadead2acef4517c1f727cce34bba894eb0a
Valentina G <valentinagaleote@gmail.com>
2024-02-02 18:47:05 +0100

    Diseño tabs

--- PracticaLayouts/winTabControl.xaml.cs
+++ PracticaLayouts/winTabControl.xaml.cs
@@ -23,5 +23,40 @@ namespace PracticaLayouts
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            rbCochazo.IsChecked = true;
+            cbPelis.SelectedIndex = 0;
+        }
+
+        private void rbCochazo_Checked(object sender, RoutedEventArgs e)
+        {
+            // A través de sender averiguamos donde se ha hecho click 
+            RadioButton misender = ((RadioButton)sender);
+            string laimagen = "";
+            switch (misender.Name)
+            { // Escogemos la imagen
+                case "rbCochazo":
+                    laimagen = "audi.jpg";
+                    //laimagen = @"cochesypelis\audi.jpg"; //Si está en carpeta así 
+                    break;
+                case "rbAsequible":
+                    laimagen = "jaguar.jpg";
+                    break;
+                case "rbAceptable":
+                    laimagen = "beetle.jpg";
+                    break;
+            }
+            try
+            {
+                imgCoche.Source = //asignamos archivo de imagen a mostrar 
+                new BitmapImage(new Uri(laimagen, UriKind.Relative));
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
     }
 }
